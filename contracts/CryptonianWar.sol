cc7423894e8cfd111dc0b20d908ece1859f69973
Ilamini Dagogo <dagogodboss@gmail.com>
2022-08-04 13:41:49 +0100

    Mint many to many

--- contracts/CryptonianWar.sol
+++ contracts/CryptonianWar.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-
 import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
@@ -9,14 +8,22 @@ import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol"
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155ReceiverUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract CryptonianWar is Initializable, ERC1155Upgradeable, AccessControlUpgradeable, PausableUpgradeable, ERC1155BurnableUpgradeable, ERC1155SupplyUpgradeable {
+contract CryptonianWar is
+    Initializable,
+    ERC1155Upgradeable,
+    AccessControlUpgradeable,
+    PausableUpgradeable,
+    ERC1155BurnableUpgradeable,
+    ERC1155SupplyUpgradeable,
+    IERC1155ReceiverUpgradeable
+{
     using SafeMathUpgradeable for uint256;
     using CountersUpgradeable for CountersUpgradeable.Counter;
     CountersUpgradeable.Counter private _listId;
 
-    uint256 public collectibleId;
     bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant LISTER_ROLE = keccak256("LISTER_ROLE");
@@ -29,9 +36,14 @@ contract CryptonianWar is Initializable, ERC1155Upgradeable, AccessControlUpgrad
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(URI_SETTER_ROLE, msg.sender);
+        _grantRole(LISTER_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    function initialize() initializer public {
+    function initialize() public initializer {
         __ERC1155_init("https://dagogo.ipfs.net");
         __AccessControl_init();
         __Pausable_init();
@@ -44,69 +56,121 @@ contract CryptonianWar is Initializable, ERC1155Upgradeable, AccessControlUpgrad
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    function mint(address _receiver, uint256 amount) public {
-        uint256 cost = 0.05 ether;
-        if(WhiteList[msg.sender] > 0){
-            payable(msg.sender).transfer(cost.mul(amount));
-           interMint(_receiver, amount);
-        }else{
-            payable(address(this)).transfer(cost.add(1).mul(amount));
-            interMint(_receiver, amount);
+    function mint(
+        address[] memory _receiver,
+        uint256[] memory amount,
+        uint256[] memory ids
+    ) public onlyRole(MINTER_ROLE) returns(bool status) {
+        for (uint256 i = 0; i < _receiver.length; i++) {
+            _mint(_receiver[i], ids[i], amount[i], "");
         }
     }
 
-    function whiteList(address[] memory allowedList) public onlyRole(LISTER_ROLE){
-        for(uint i=0; i<allowedList.length; i++) {
-            if(WhiteList[allowedList[i]]==0) { 
+    function mintManyToMany(
+        address[] memory _receiver,
+        uint256[] memory amounts,
+        uint256[] memory ids
+    ) public onlyRole(MINTER_ROLE) {
+        _mintBatch(msg.sender, ids, amounts, "");
+        setApprovalForAll(address(this), true);
+        // batch transafer nft  sc to receivers
+        for (uint256 i = 0; i < _receiver.length; i++) {
+            safeTransferFrom(msg.sender, _receiver[i], ids[i], amounts[i], "");
+        }
+    }
+
+    function whiteList(address[] memory allowedList)
+        public
+        onlyRole(LISTER_ROLE)
+    {
+        for (uint256 i = 0; i < allowedList.length; i++) {
+            if (WhiteList[allowedList[i]] == 0) {
                 _listId.increment();
                 WhiteList[allowedList[i]] = _listId.current();
             }
         }
     }
 
-     function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE)  {
+    function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
         _setURI(newuri);
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-    {
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public {
         _mintBatch(to, ids, amounts, data);
     }
 
-    
     // The following functions are overrides required by Solidity.
 
-
-    function interMint(address _receiver, uint256 amount) internal {
-        _mint(_receiver, collectibleId, amount, "");
-        collectibleId++;
-    }
-    function pause() public  onlyRole(PAUSER_ROLE) {
+    function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
-    function unpause() public  onlyRole(PAUSER_ROLE) {
+    function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
 
-    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    )
         internal
-        whenNotPaused
         override(ERC1155Upgradeable, ERC1155SupplyUpgradeable)
+        whenNotPaused
     {
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 
-    
     // The following functions are overrides required by Solidity.
 
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC1155Upgradeable, AccessControlUpgradeable)
+        override(
+            ERC1155Upgradeable,
+            AccessControlUpgradeable,
+            IERC165Upgradeable
+        )
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external override(IERC1155ReceiverUpgradeable) returns (bytes4) {
+        return
+            bytes4(
+                keccak256(
+                    "onERC1155Received(address,address,uint256,uint256,bytes)"
+                )
+            );
+    }
+
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata ids,
+        uint256[] calldata values,
+        bytes calldata data
+    ) external override(IERC1155ReceiverUpgradeable) returns (bytes4) {
+        return
+            bytes4(
+                keccak256(
+                    "onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"
+                )
+            );
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,90 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { ContractFactory } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+import { CryptonianWar } from "../typechain/index";
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+let warContract: CryptonianWar,
+  deployer: SignerWithAddress,
+  userOne: SignerWithAddress,
+  userTwo: SignerWithAddress,
+  userThree: SignerWithAddress,
+  userFour: SignerWithAddress,
+  userFive: SignerWithAddress;
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+before(async () => {
+  [deployer, userOne, userTwo, userThree, userFour, userFive] =
+    await ethers.getSigners();
+  const CryptonianWar = await ethers.getContractFactory("CryptonianWar");
+  warContract = (await CryptonianWar.deploy()) as CryptonianWar;
+  await warContract.deployed();
+});
+describe("Cryptonian War", function () {
+  it("has URI", async function () {
+    expect(await warContract.uri(0)).to.be.string("");
+  });
+});
+describe("Check Roles has being assigned", function () {
+  it("should have set url role", async () => {
+    expect(
+      await warContract.hasRole(
+        await warContract.URI_SETTER_ROLE(),
+        deployer.address
+      )
+    ).to.equal(true);
+  });
+  it("should have LISTER role", async () => {
+    expect(
+      await warContract.hasRole(
+        await warContract.LISTER_ROLE(),
+        deployer.address
+      )
+    ).to.equal(true);
+  });
+  it("should have PAUSER role", async () => {
+    expect(
+      await warContract.hasRole(
+        await warContract.PAUSER_ROLE(),
+        deployer.address
+      )
+    ).to.equal(true);
+  });
+  it("should have PAUSER role", async () => {
+    expect(
+      await warContract.hasRole(
+        await warContract.MINTER_ROLE(),
+        deployer.address
+      )
+    ).to.equal(true);
+  });
+  it("expect deployer to have all roles", async () => {});
+});
+describe("NFT Minting", async () => {
+  it("should mint many nfts to many addresses", async () => {
+    const mint = warContract.mintManyToMany(
+      [
+        userOne.address,
+        userTwo.address,
+        userThree.address,
+        userFour.address,
+        userFive.address,
+      ],
+      [2, 2, 1, 1, 2],
+      [1, 2, 3, 4, 5]
+    );
+    expect(await mint).emit("ERC1155Upgradeable", "TransferSingle");
+  });
+  it("should update the balance of the userOne", async () => {
+    await expect(await warContract.balanceOf(userOne.address, 1)).eq(2);
+  });
+  it("should update the balance of the userTow", async () => {
+    await expect(await warContract.balanceOf(userTwo.address, 2)).eq(2);
+  });
+  it("should update the balance of the userThree", async () => {
+    await expect(await warContract.balanceOf(userThree.address, 3)).eq(1);
+  });
+  it("should update the balance of the userFour", async () => {
+    await expect(await warContract.balanceOf(userFour.address, 4)).eq(1);
   });
 });
